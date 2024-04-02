f641058a875f60f4e87efa68ef66b8cf82e40ed4
hibi <69405847+hibiii@users.noreply.github.com>
2024-04-01 21:23:47 -0300

    Add basic testing framework

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -e
+mkdir -p test_artifacts
+jshell test.jshell > test_artifacts/java_reference.txt
+lua5.2 test.lua > test_artifacts/lua_reference.txt
+diff test_artifacts/java_reference.txt test_artifacts/lua_reference.txt
+echo "Tests OK"
